f183f4fa9907890f1f0e21e2b041485740aaaaa8
Scott Rubalcaba <pythonism007@gmail.com>
2024-11-27 19:10:21 -0600

    Fixes Next and Previous buttons, and cleans up code

--- src/Components/Trivia.jsx
+++ src/Components/Trivia.jsx
@@ -1,161 +1,55 @@
 import { useState } from "react";
 
-function Trivia({questions, question, optionA, optionB, optionC, optionD, correctAnswer}){
-     
+function Trivia({ questions }) {
     const [answered, setAnswered] = useState(false);
     const [currentIndex, setCurrentIndex] = useState(0);
     const currentQuestion = questions[currentIndex];
 
-    function handleButtonClick(buttonMessage) {
-        console.log(buttonMessage)
-        setAnswered(!answered)
-        console.log(answered)
+    const handleAnswerClick = (selectedOption) => {
+        console.log('Selected: ${selectedOption}')
+        setAnswered(true)
     };
 
     const handleNext = () => {
+        setAnswered(false);
         setCurrentIndex((prevIndex) => (prevIndex + 1) % questions.length);
     };
 
     const handlePrevious = () => {
+        setAnswered(false);
         setCurrentIndex((prevIndex) => 
           prevIndex === 0 ? questions.length - 1 : prevIndex - 1
         );
       };    
-
-    function reveal(answerButton, answerDisplay) {
-    }
     
-    if (currentIndex === 0) {
-        return (
-            <div className="card">
-                <div className="card-top">
-                    <p>{currentQuestion.question}</p>
-                </div>
-                <div 
-                className="cardBottom">
-                    {!answered && 
-                        <>
-                            <button className='answerButton'
-                            onClick={()=>{handleButtonClick(optionA) 
-                            }}>A. {currentQuestion.optionA}</button>
+      const renderOptions = () => {
+        const options = ["A", "B", "C", "D"];
+        return options.map((opt, index) => (
+            <button
+                key={opt}
+                className={answered ? "answerDisplay" : "answerButton"}
+                onClick={() => !answered && handleAnswerClick(currentQuestion[`option${opt}`])}
+            >
+                {opt}. {currentQuestion[`option${opt}`]}
+            </button>
+        ));
+    };
 
-                            <button className='answerButton'
-                            onClick={()=>{handleButtonClick(optionB) 
-                            }}>B. {currentQuestion.optionB}</button>
-
-                            <button className='answerButton'
-                            onClick={()=>{handleButtonClick(optionC)
-                            }}>C. {currentQuestion.optionC}</button>
-
-                            <button className='answerButton'
-                            onClick={()=>{handleButtonClick(optionD)
-                            }}>D. {currentQuestion.optionD}</button>
-
-                        </>
-                    }
-                    {answered &&
-                        <div className="answers">
-                            <button className='answerDisplay'>A. {optionA}</button>
-                            <button className='answerDisplay'>B. {optionB}</button>
-                            <button className='answerDisplay'>C. {optionC}</button>
-                            <button className='answerDisplay'>D. {optionD}</button>
-                        </div>
-                    }
-                </div>
-                <div>
-                    <button onClick={handleNext}>Next</button>
-                </div>
+    return (
+        <div className="card">
+            <div className="card-top">
+                <p>{currentQuestion.question}</p>
             </div>
-            )
-        } else if (currentIndex === questions.length - 1) {
-            return (
-                <div className="card">
-                    <div className="card-top">
-                        <p>{currentQuestion.question}</p>
-                    </div>
-                    <div 
-                    className="cardBottom">
-                        {!answered && 
-                            <>
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionA) 
-                                }}>A. {currentQuestion.optionA}</button>
-
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionB) 
-                                }}>B. {currentQuestion.optionB}</button>
-
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionC)
-                                }}>C. {currentQuestion.optionC}</button>
-
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionD)
-                                }}>D. {currentQuestion.optionD}</button>
-
-                            </>
-                        }
-                        {answered &&
-                            <div className="answers">
-                                <button className='answerDisplay'>A. {optionA}</button>
-                                <button className='answerDisplay'>B. {optionB}</button>
-                                <button className='answerDisplay'>C. {optionC}</button>
-                                <button className='answerDisplay'>D. {optionD}</button>
-                            </div>
-                        }
-                    </div>
-                    <div>
-                        <button onClick={handlePrevious}>Previous</button>
-                        <button onClick={handleNext}>Next</button>
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-                <div className="card">
-                    <div className="card-top">
-                        <p>{currentQuestion.question}</p>
-                    </div>
-                    <div 
-                    className="cardBottom">
-                        {!answered && 
-                            <>
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionA) 
-                                }}>A. {currentQuestion.optionA}</button>
-
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionB) 
-                                }}>B. {currentQuestion.optionB}</button>
-
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionC)
-                                }}>C. {currentQuestion.optionC}</button>
-
-                                <button className='answerButton'
-                                onClick={()=>{handleButtonClick(optionD)
-                                }}>D. {currentQuestion.optionD}</button>
-
-                            </>
-                        }
-                        {answered &&
-                            <div className="answers">
-                                <button className='answerDisplay'>A. {optionA}</button>
-                                <button className='answerDisplay'>B. {optionB}</button>
-                                <button className='answerDisplay'>C. {optionC}</button>
-                                <button className='answerDisplay'>D. {optionD}</button>
-                            </div>
-                        }
-                    </div>
-                    <div>
-                        <button onClick={handlePrevious}>Previous</button>
-                    </div>
-                </div>
-            )
-        }
+            <div className="cardBottom">
+                {renderOptions()}
+            </div>
+            <div>
+                <button onClick={handlePrevious}>Previous</button>
+                <button onClick={handleNext}>Next</button>
+            </div>
+        </div>
+    )
 }                    
-
-
 
 
 // style={{ display: index == activeIndex ? "block" : "none" }}
